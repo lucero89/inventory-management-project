ab63944453c31f9834004cc719770a31e0f7e559
lucero89 <laila.aquillos@gmail.com>
2022-11-08 18:54:06 -0300

    git commit initial

---
+++ src/main/java/org/ada/inventorymanagerproject/InventoryManagerProjectApplication.java
@@ -0,0 +1,13 @@
+package org.ada.inventorymanagerproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ada/inventorymanagerproject/InventoryManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.inventorymanagerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
